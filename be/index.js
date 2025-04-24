ebb85fe8ea4d1064a179f7e249a7c923cb378144
nihal-soni <nihalsoni0523@gmail.com>
2025-04-25 00:03:48 +0530

    chore: set up Express server with middleware and database connection

--- be/index.js
+++ be/index.js
@@ -1,13 +1,33 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app = express();
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message:"hi there this is just a checking message",
+        success:true
+
+    })
+})
+//middleware 
+app.use(express.json())
+app.use(cookieParser())
+app.use(urlencoded({extended:true}));
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions))
 
 
-
-const PORT = 3000
 
 app.listen(PORT, () => {
+    connectDB()
     console.log(`server is running on port ${PORT}`)
 })
